a4fa22cfd94f7e4e9f284dff9978149434c32891
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-05-28 15:20:05 +0200

    [#2] Add the master view of the application

--- Sources/Odin/Odin/Views/CalculatorView.swift
+++ Sources/Odin/Odin/Views/CalculatorView.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
+import Model
+
+struct CalculatorView: View {
+    public var odinVM: OdinVM
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 0) {
+                    BlocsView(blocs: odinVM.blocs)
+                        .padding(.top, 10)
+                        .padding(.horizontal, 6)
+                    
+                    Divider()
+                        .frame(height: 1)
+                        .overlay(.gray)
+                        .padding(.vertical, 16)
+                        
+                    TeachingUnitsView(odinVM: odinVM)
+                        .padding(.horizontal, 6)
+                }
+                .navigationTitle("Calculette")
+            }
+        }
+    }
+}
+
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView(odinVM: OdinVM(withTeachingUnits: generateOdin().teachingUnits, withBlocs: generateOdin().blocs))
+    }
+}
